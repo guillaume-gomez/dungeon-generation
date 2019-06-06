42b4307b3329c4950cabc68e7039619cde01bb9d
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-06-06 08:19:04 +0200

    update algorithm to add rooms

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int openingDirection;
+    // 1 bottom door
+    // 2 top door
+    // 3 left door
+    // 4 right door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start() {
+      templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+      Invoke("Spawn", 1.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
+      if(spawned) {
+        return;
+      }
+
+      switch(openingDirection) {
+        case 1:
+          //  bottom
+          instanciateRoom(templates.topRooms);
+        break;
+        case 2:
+          //  top
+          instanciateRoom(templates.bottomRooms);
+        break;
+        case 3:
+          //  left
+          instanciateRoom(templates.rightRooms);
+        break;
+        case 4:
+          //  right
+          instanciateRoom(templates.leftRooms);
+        break;
+      }
+      spawned = true;
     }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.CompareTag("SpawnPoint")) {
+      if(other.GetComponent<RoomSpawner>() && other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+      {
+        Debug.Log("closedRoom");
+        Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+      }
+      spawned = true;
+    }
+  }
+
+  private void instanciateRoom(GameObject[] rooms) {
+    rand = Random.Range(0, rooms.Length);
+    GameObject room = rooms[rand];
+    Instantiate(room, transform.position, room.transform.rotation);
+  }
 }
