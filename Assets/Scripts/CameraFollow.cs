86f4d2d13204e05582b67350a452f24020770702
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-06-11 00:50:41 +0200

    add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float dampTime = 0.4f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+      cameraPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+      transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
