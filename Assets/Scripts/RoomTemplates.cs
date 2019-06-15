1e728817e6569c61c330356feade0c336cabc824
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-06-15 02:03:23 +0200

    store rooms to know where to put boss

--- Assets/Scripts/RoomTemplates.cs
+++ Assets/Scripts/RoomTemplates.cs
@@ -10,4 +10,22 @@ public class RoomTemplates : MonoBehaviour
   public GameObject[] rightRooms;
 
   public GameObject closedRoom;
+
+  public List<GameObject> rooms;
+
+  public float waitTime = 2.0f;
+  private bool spawnedBoss;
+  public GameObject boss;
+
+
+  void Update() {
+    if(waitTime <= 0 && spawnedBoss == false) {
+      GameObject lastRoom = rooms[rooms.Count - 1];
+      Instantiate(boss, lastRoom.transform.position, Quaternion.identity);
+      spawnedBoss = true;
+    } else {
+      waitTime -= Time.deltaTime;
+    }
+  }
+
 }
