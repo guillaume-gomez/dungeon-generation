b45b1e37fd3c4fcdbdd28cbae448fc3fcaccb871
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-06-17 09:13:23 +0200

    add new script to store info about room

---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    private List<GameObject> parents;
+    private RoomTemplates templates;
+
+    void Start() {
+      // add room in template
+      templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+      templates.rooms.Add(gameObject);
+    }
+
+    public void AddParent(GameObject parent) {
+      parents.Add(parent);
+    }
+
+    public void RemoveParent(GameObject parent) {
+      parents.Remove(parent);
+    }
+
+    public void RemoveAllParent() {
+      parents.Clear();
+    }
+
+    public bool HasParent() {
+      return parents.Count > 0;
+    }
+}
