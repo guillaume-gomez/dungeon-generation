5a28c489a561fc764f4a45c1b4343d803d63120c
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-06-27 00:11:29 +0200

    check with spawner we need to restart

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class RoomScript : MonoBehaviour
 {
     private List<GameObject> parents;
+    private List<GameObject> children;
+    private List<string> spawnersName;
     private RoomTemplates templates;
+
+    void Awake() {
+      parents = new List<GameObject>();
+      children = new List<GameObject>();
+      // get the spawners
+      foreach (Transform child in transform)
+      {
+        if(child.name.Constains("SpawnPoint")) {
+          spawners.Add(child.name);
+        }
+      }
+    }
 
     void Start() {
       // add room in template
@@ -25,7 +39,67 @@ public class RoomScript : MonoBehaviour
       parents.Clear();
     }
 
+    public void AddChild(GameObject child) {
+      children.Add(child);
+    }
+
+    public void RemoveChild(GameObject child) {
+      children.Remove(child);
+    }
+
+    public void RemoveAllChildren() {
+      children.Clear();
+    }
+
     public bool HasParent() {
-      return parents.Count > 0;
+      return NbParents() > 0;
+    }
+
+    public int NbParents() {
+      return parents.Count;
+    }
+
+    public bool HasChildren() {
+      return NbChildren() > 0;
+    }
+
+    public int NbChildren() {
+      return children.Count;
+    }
+
+    public void Remove() {
+      foreach(GameObject parent in parents) {
+        foreach(Transform child in parent.transform) {
+          if(child.CompareTag("SpawnPoint")) {
+            switch(child.name) {
+              case "SpawnPointTop":
+                if(spawners.Contains("SpawnPointBottom")) {
+                  //RoomSpawner spawner = child.GetComponent<RoomSpawner>();
+                  //spawner.Restart();
+                }
+              break;
+              case "SpawnPointBottom":
+                if(spawners.Contains("SpawnPointTop")) {
+                  //RoomSpawner spawner = child.GetComponent<RoomSpawner>();
+                  //spawner.Restart();
+                }
+              break;
+              case "SpawnPointLeft":
+                if(spawners.Contains("SpawnPointRight")) {
+                  //RoomSpawner spawner = child.GetComponent<RoomSpawner>();
+                  //spawner.Restart();
+                }
+              break;
+              case "SpawnPointRight":
+                if(spawners.Contains("SpawnPointLeft")) {
+                  //RoomSpawner spawner = child.GetComponent<RoomSpawner>();
+                  //spawner.Restart();
+                }
+              break;
+            }
+          }
+        }
+      }
+      Debug.Log(gameObject.name);
     }
 }
